bacd76f930f318a7a285eac2c5b2799c4dc37603
gutbrot <159773930+gutbrot@users.noreply.github.com>
2024-03-07 17:50:07 +0000

    chapter 1

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getPersonalisedHello(name: string, day: string){
+    return `Hello ${name}, have a nice day ${day}!`;
+  }
 }
